a2bb22db4191ce1c3790df4af32f0e5d76ab9e2e
Greg Stupp <gstupp@scripps.edu>
2016-11-22 17:30:10 -0800

    interpro bot release from command line

--- scheduled_bots/interpro/bot.py
+++ scheduled_bots/interpro/bot.py
@@ -22,16 +22,17 @@ if __name__ == "__main__":
     parser.add_argument('--log-dir', help='directory to store logs', type=str)
     parser.add_argument('--run-one', help='run one doc', type=str)
     parser.add_argument('--dummy', help='do not actually do write', action='store_true')
-    parser.add_argument('--mongo-uri', type=str)
     parser.add_argument('--interpro-version', type=str)
     parser.add_argument('--interpro-date', type=str)
     args = parser.parse_args()
     log_dir = args.log_dir if args.log_dir else "./logs"
     login = wdi_login.WDLogin(user=WDUSER, pwd=WDPASS)
 
-    # TODO: get this from biothings/command line
-    version_date = date_parse("03-NOV-16")
-    version_num = "60.0"
+    version_date = date_parse(args.interpro_date)
+    version_num = args.interpro_version
+
+    #version_date = date_parse("03-NOV-16")
+    #version_num = "60.0"
 
     release = wdi_helpers.Release(title="InterPro Release {}".format(version_num),
                                   description="Release {} of the InterPro database & software".format(version_num),
