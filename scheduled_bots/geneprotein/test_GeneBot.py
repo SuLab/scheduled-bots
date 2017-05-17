15c4c8329b895f48530b8a098d73947df957cbf9
Greg Stupp <gstupp@scripps.edu>
2017-05-17 14:24:21 -0700

    update genebot tests

--- scheduled_bots/geneprotein/test_GeneBot.py
+++ scheduled_bots/geneprotein/test_GeneBot.py
@@ -6,50 +6,44 @@ from scheduled_bots.geneprotein.GeneBot import main, wdi_core
 from pymongo import MongoClient
 
 
-def test_write_one_human_gene():
-    # https://www.wikidata.org/wiki/Q14911732
+def _test_write_one_gene(qid, entrezgene, taxid):
     coll = MongoClient().wikidata_src.mygene
     metadata_coll = MongoClient().wikidata_src.mygene_sources
     metadata = metadata_coll.find_one()
-    doc_filter = {'_id': '1017'}
+    doc_filter = {'_id': entrezgene}
     print("total number of records: {}".format(coll.find(doc_filter).count()))
 
-    main(coll, taxid='9606', metadata=metadata, fast_run=False, write=True, doc_filter=doc_filter)
+    main(coll, taxid=taxid, metadata=metadata, fast_run=False, write=True, doc_filter=doc_filter)
 
     fn = wdi_core.WDItemEngine.logger.handlers[0].baseFilename
     log = open(fn).read()
-    assert "Q14911732" in log
+    assert qid in log
     assert "WARNING" not in log and "ERROR" not in log
 
+
+def test_write_one_human_gene():
+    qid = "Q14911732"
+    taxid = '9606'
+    entrezgene = '1017'
+    _test_write_one_gene(qid, entrezgene, taxid)
 
 
 def test_write_one_microbe_gene():
-    # https://www.wikidata.org/wiki/Q23124687
-    coll = MongoClient().wikidata_src.mygene
-    metadata_coll = MongoClient().wikidata_src.mygene_sources
-    metadata = metadata_coll.find_one()
-    doc_filter = {'_id': '2614876'}
-    print("total number of records: {}".format(coll.find(doc_filter).count()))
-
-    main(coll, taxid='243277', metadata=metadata, fast_run=False, write=True, doc_filter=doc_filter)
-
-    fn = wdi_core.WDItemEngine.logger.handlers[0].baseFilename
-    log = open(fn).read()
-    assert "Q23124687" in log
-    assert "WARNING" not in log and "ERROR" not in log
+    qid = "Q23124687"
+    taxid = '243277'
+    entrezgene = '2614876'
+    _test_write_one_gene(qid, entrezgene, taxid)
 
 
 def test_write_one_yeast_gene():
-    # https://www.wikidata.org/wiki/Q27539996
-    coll = MongoClient().wikidata_src.mygene
-    metadata_coll = MongoClient().wikidata_src.mygene_sources
-    metadata = metadata_coll.find_one()
-    doc_filter = {'_id': '856002'}
-    print("total number of records: {}".format(coll.find(doc_filter).count()))
+    qid = "Q27539996"
+    taxid = '559292'
+    entrezgene = '856002'
+    _test_write_one_gene(qid, entrezgene, taxid)
 
-    main(coll, taxid='559292', metadata=metadata, fast_run=False, write=True, doc_filter=doc_filter)
 
-    fn = wdi_core.WDItemEngine.logger.handlers[0].baseFilename
-    log = open(fn).read()
-    assert "Q27539996" in log
-    assert "WARNING" not in log and "ERROR" not in log
+def test_write_one_mouse_gene():
+    qid = "Q18309250"
+    taxid = '10090'
+    entrezgene = '12566'
+    _test_write_one_gene(qid, entrezgene, taxid)
