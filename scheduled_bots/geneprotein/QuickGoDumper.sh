e532d7fdc02e66b202451488fc14be0ca98ee857
Greg Stupp <gstupp@scripps.edu>
2017-11-30 16:57:26 -0800

    save retrieved

--- scheduled_bots/geneprotein/QuickGoDumper.sh
+++ scheduled_bots/geneprotein/QuickGoDumper.sh
@@ -8,6 +8,7 @@ FILEOUT='goa_uniprot_all_filt.gaf.gz'
 
 # delete trigger if exists
 rm -f -- TRIGGER
+rm -f -- RETRIEVED
 
 # check if download is needed
 ls -l
@@ -24,18 +25,17 @@ if [ "$CURRENT_TS" == "$NEW_TS" ]; then
     echo "time stamps match, exiting"
     exit 0
 fi
+RETRIEVED=$(date -r $FILE +%Y%m%d)
+echo $RETRIEVED > RETRIEVED
 
 # if we haven't exited, proceed with file parsing
 
 # cut off the header and select only the columns we want
 zcat $FILE | grep -v '^!' | awk 'BEGIN{FS="\t"}{print $1,$2,$5,$6,$7,$9,$13,$15}' | gzip > $FILEOUT
-#mv tmp2.gz tmp.gz
 
 # to select human for example
 #zcat $FILEOUT | awk '$7=="taxon:9606"' > 9606.gaf
 #zcat $FILEOUT | awk '$7=="taxon:35758"' > 35758.gaf
-
-
 
 touch TRIGGER
 ls -l
