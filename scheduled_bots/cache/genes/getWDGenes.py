ee70c1718b1af70e9152cbd7b35987b097482cb6
andrawaag <andra@waagmeester.net>
2019-09-17 23:19:33 +0200

    test script to cache Genewiki content

---
+++ scheduled_bots/cache/genes/getWDGenes.py
@@ -0,0 +1,24 @@
+from wikidataintegrator import wdi_core
+import pandas as pd
+from rdflib import Graph
+import time
+import sys
+query = """
+SELECT * WHERE {
+   ?item wdt:P31 wd:Q7187 .
+}
+"""
+kg = Graph()
+kg.
+results = wdi_core.WDItemEngine.execute_sparql_query(query)
+i =0
+for qid in results["results"]["bindings"]:
+    try:
+        # print(qid["item"]["value"].replace("http://www.wikidata.org/entity/", ""))
+        kg.parse(qid["item"]["value"]+".ttl")
+        i+=1
+        print(i)
+    except:
+        print(print(qid["item"]["value"].replace("http://www.wikidata.org/entity/", "")))
+        time.sleep(5)
+kg.serialize(destination="genes.ttl", format="turtle")
