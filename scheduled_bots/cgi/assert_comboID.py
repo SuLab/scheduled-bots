fe898b0544e0026a06c2cd16790cbb3dbe02b86e
Andra Waagmeester <andra@micelio.be>
2022-03-02 13:41:18 +0100

    The issue with emerging combinatorial therapies can now be fixed with an adaption to the script that identifies where the assersion issue is. Now this can be fixed automatically.

--- scheduled_bots/cgi/assert_comboID.py
+++ scheduled_bots/cgi/assert_comboID.py
@@ -1,5 +1,5 @@
-from wikidataintegrator import wdi_core
-import pprint
+from wikidataintegrator import wdi_core, wdi_login
+import os
 
 # get existing combinations:
 query_str = """SELECT ?item ?itemLabel (GROUP_CONCAT(?part; separator=";") as ?f) WHERE {
@@ -12,8 +12,30 @@ combo_qid = {x['item']['value'].replace("http://www.wikidata.org/entity/", ""): 
 qid_combo = {v:k for k,v in combo_qid.items()}
 #assert len(combo_qid) == len(qid_combo)
 
+print("Logging in...")
+if "WDUSER" in os.environ and "WDPASS" in os.environ:
+    WDUSER = os.environ['WDUSER']
+    WDPASS = os.environ['WDPASS']
+else:
+    raise ValueError("WDUSER and WDPASS must be specified in local.py or as environment variables")
+
+login = wdi_login.WDLogin(WDUSER, WDPASS)
+solved =[]
 for qid1 in combo_qid.keys():
+    if qid1 in solved:
+        continue
     for qid2 in combo_qid.keys():
         if qid1 != qid2:
             if combo_qid[qid1] == combo_qid[qid2]:
                 print(qid1, combo_qid[qid1], ":", qid2, combo_qid[qid2])
+                print(qid1[1:])
+                if int(qid1[1:]) > int(qid2[1:]):
+                    source = qid2
+                    target = qid1
+                else:
+                    source = qid2
+                    target = qid1
+                wdi_core.WDItemEngine.merge_items(source, target, login)
+                solved.append(qid1)
+                solved.append(qid2)
+
