dbaf2431995abab8b679e78fe6067262e3e551bf
Andra Waagmeester <andra@micelio.be>
2022-05-04 14:09:08 +0200

    Integrated the fix for #69 directly in the bot, so the check and possible fix will run directly from the bot

--- scheduled_bots/cgi/bot.py
+++ scheduled_bots/cgi/bot.py
@@ -24,6 +24,37 @@ except ImportError:
     else:
         raise ValueError("WDUSER and WDPASS must be specified in local.py or as environment variables")
 
+## First check if combinatorial therapies have duplicates on Wikidata and merge them before continuing
+# get existing combinations:
+query_str = """SELECT ?item ?itemLabel (GROUP_CONCAT(?part; separator=";") as ?f) WHERE {
+  ?item wdt:P527 ?part .
+  ?item wdt:P31 wd:Q1304270 .
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+} GROUP BY ?item ?itemLabel"""
+results = wdi_core.WDItemEngine.execute_sparql_query(query_str)['results']['bindings']
+combo_qid = {x['item']['value'].replace("http://www.wikidata.org/entity/", ""): frozenset([y.replace("http://www.wikidata.org/entity/", "") for y in x['f']['value'].split(";")]) for x in results}
+qid_combo = {v:k for k,v in combo_qid.items()}
+
+login = wdi_login.WDLogin(WDUSER, WDPASS)
+solved =[]
+for qid1 in combo_qid.keys():
+    if qid1 in solved:
+        continue
+    for qid2 in combo_qid.keys():
+        if qid1 != qid2:
+            if combo_qid[qid1] == combo_qid[qid2]:
+                print(qid1, combo_qid[qid1], ":", qid2, combo_qid[qid2])
+                print(qid1[1:])
+                if int(qid1[1:]) > int(qid2[1:]):
+                    source = qid2
+                    target = qid1
+                else:
+                    source = qid2
+                    target = qid1
+                wdi_core.WDItemEngine.merge_items(source, target, login)
+                solved.append(qid1)
+                solved.append(qid2)
+
 # map association column
 association_map = {
     'Responsive': 'P3354',  # positive therapeutic predictor
